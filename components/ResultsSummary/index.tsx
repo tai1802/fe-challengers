dd512d091583a648f51cf93fc248c2ac13494921
Tai Nguyen <tai.nguyen@mgisolutions.com>
2025-02-07 11:01:48 +0700

    add challenger 6

--- components/ResultsSummary/index.tsx
+++ components/ResultsSummary/index.tsx
@@ -2,8 +2,8 @@ import SummaryItem from "./SummaryItem";
 
 const ResultsSummary = () => {
   return (
-    <div className="bg-white shadow-2xl m-auto max-w-xl rounded-3xl flex flex-row">
-      <div className="bg-gradient-to-t from-gradients-light-royal-blue from-35% via-gradients-light-slate-blue via-100% max-w-80 h-96 rounded-3xl p-8 flex flex-col gap-5 min-w-72">
+    <div className="bg-white md:shadow-2xl mx-auto md:m-auto max-w-[640px] rounded-b-3xl rounded-t-none md:rounded-3xl flex flex-col md:flex-row">
+      <div className="bg-gradient-to-t from-gradients-light-royal-blue from-35% via-gradients-light-slate-blue via-100% max-w-96 md:max-w-80 rounded-b-3xl rounded-t-none md:rounded-3xl p-8 flex flex-col gap-5 min-w-72">
         <p className="text-neutral-light-lavender text-center text-lg">Your Result</p>
         <div className="p-6 rounded-full bg-gradient-to-b from-gradients-violet-blue from-20% via-gradients-persian-blue via-100% min-w-32 min-h-32 flex mx-auto">
           <div className="m-auto">
@@ -12,9 +12,11 @@ const ResultsSummary = () => {
           </div>
         </div>
         <p className="text-3xl text-neutral-pale-blue text-center">Great</p>
-        <p className="text-center text-neutral-light-lavender text-base">You scored higher than 65% of the people who have taken these tests.</p>
+        <p className="text-center text-neutral-light-lavender text-base">
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
       </div>
-      <div className="min-w-72 p-8 space-y-6">
+      <div className="min-w-80 p-8 space-y-6">
         <p className="text-xl font-extrabold text-neutral-dark-gray-blue">Summary</p>
         <div className="flex flex-col gap-4">
           <SummaryItem iconPath="/icon-reaction.svg" name="Reaction" score={80} color="light-red" />
@@ -22,6 +24,9 @@ const ResultsSummary = () => {
           <SummaryItem iconPath="/icon-verbal.svg" name="Verbal" score={61} color="green-teal" />
           <SummaryItem iconPath="/icon-visual.svg" name="Visual" score={72} color="cobalt-blue" />
         </div>
+        <button className="py-4 bg-neutral-dark-gray-blue w-full rounded-full text-white hover:bg-gradient-to-t hover:from-gradients-light-royal-blue hover:from-35% hover:via-gradients-light-slate-blue hover:via-100%">
+          Continue
+        </button>
       </div>
     </div>
   );
